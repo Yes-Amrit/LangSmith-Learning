1a2967b40a3b42f8f4213716241c7ee1fb45e285
Yes-Amrit <amugi2712@gmail.com>
2026-06-06 15:26:01 +0530

    Langsmith Demo using Simple RAG

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -1,14 +1,19 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
+import os
 load_dotenv()
 
 # Simple one-line prompt
 prompt = PromptTemplate.from_template("{question}")
 
-model = ChatOpenAI()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=GOOGLE_API_KEY,
+    temperature=0.7
+)
 parser = StrOutputParser()
 
 # Chain: prompt → model → parser
